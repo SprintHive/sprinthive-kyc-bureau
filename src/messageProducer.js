ac7c3ee77d9733152d303928da27abf3a36069dd
Jon Lai Lam <jon@lailam.co.za>
2018-04-11 12:29:21 +0200

    Added LastName

--- src/messageProducer.js
+++ src/messageProducer.js
@@ -58,6 +58,7 @@ const sendSuccessMessagesToRabbit = (action$) => {
           alternateProviderIdentifyingNumber: "",
           firstName: ConsumerDetail.FirstName,
           middleNames: getMiddleNames(ConsumerDetail),
+          lastName: ConsumerDetail.LastName,
           dateOfBirth: ConsumerDetail.BirthDate,
           deceased: ConsumerFraudIndicatorsSummary.HomeAffairsDeceasedStatus === "Yes",
           verifiedWithHomeAffairs: ConsumerFraudIndicatorsSummary.HomeAffairsVerificationYN === "Yes"
